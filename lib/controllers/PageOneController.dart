358f27c2ed6bf762b5cf2aef4dab6697536ce545
Sohaib Aslam <49934444+siman302@users.noreply.github.com>
2023-03-11 20:44:45 +0500

    change text

--- lib/controllers/PageOneController.dart
+++ lib/controllers/PageOneController.dart
@@ -3,14 +3,14 @@ import 'package:get/get.dart';
 
 class PageOneController extends GetxController {
   Color color = Colors.red;
-  String phrase = "Do you respect me ?";
+  String phrase = "love mean";
   changeColor() {
     if (color == Colors.red) {
       color = Colors.yellow;
-      phrase = "Pls not repect me, u can like me";
+      phrase = "pak?";
     } else {
       color = Colors.red;
-      phrase = "I dont like people who repect me";
+      phrase = "yes";
     }
     update();
   }
